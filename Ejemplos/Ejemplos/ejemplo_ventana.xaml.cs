6231c59e1b56c850c47a220dea91015933be281e
CallmeN <95440504+CallmeCarlos503@users.noreply.github.com>
2023-01-26 22:42:52 -0600

    avance

--- Ejemplos/Ejemplos/ejemplo_ventana.xaml.cs
+++ Ejemplos/Ejemplos/ejemplo_ventana.xaml.cs
@@ -31,14 +31,46 @@ namespace Ejemplos
                 int n1, n2;
                 n1 = Convert.ToInt32(txtn1.Text);
                 n2= Convert.ToInt32(txtn2.Text);
-                MessageBox.Show("La suma de ambos valores fue de: "+(n1+n2));
+                if (n1 >=0 &&  n2>=0)
+                {
+                    MessageBox.Show("La suma de ambos valores fue de: " + (n1 + n2));
+                }
+                else
+                {
+                    MessageBox.Show("Tienen que ser valores positivos");
+                }
+                
             }
             catch (Exception ex)
             {
+                MessageBox.Show("Error: Los valores no son numericos");
 
             }
-            
 
+
+        }
+
+        private void btnRestar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int n1, n2;
+                n1 = Convert.ToInt32(txtn1.Text);
+                n2 = Convert.ToInt32(txtn2.Text);
+                if (n1 >= 0 && n2 >=0)
+                {
+                    MessageBox.Show("La resta de ambos valores fue de: " + (n1 - n2));
+                }
+                else
+                {
+                    MessageBox.Show("Tienen que ser valores mayores o iguales  a 0");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: Los valores no son numericos");
+
+            }
         }
     }
 }
